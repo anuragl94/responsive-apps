d4f2fcac71122c283c9c238002901d0b91d5fe91
Anurag Lingam <anurag.lingam@zopnow.com>
2017-11-06 01:00:54 +0530

    WIP: Social media design

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import Router from './router'
 
 import 'sanitize.css/sanitize.css'
-import './App.css'
+import './app.css'
 
 class App extends Component {
   render () {
