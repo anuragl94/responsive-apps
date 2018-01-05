dfbbe2d24684234341a7a29bcd982a4617434b30
Anurag Lingam <anurag.lingam@zopnow.com>
2018-01-05 16:11:33 +0530

    Added codeception basic test case

---
+++ tests/acceptance/FirstCest.php
@@ -0,0 +1,20 @@
+<?php
+
+
+class FirstCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+        $I->amOnPage    ('/');
+    }
+
+    public function _after(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(AcceptanceTester $I)
+    {
+        $I->see('Select an app');
+    }
+}
